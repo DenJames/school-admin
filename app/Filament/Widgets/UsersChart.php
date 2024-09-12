1a70fcdea3d22870b7eb238b4cade906d5484b16
Dennis <denn369e@edu.mercantec.dk>
2024-09-12 15:53:29 +0200

    feat: add example chart to admin dashboard

---
+++ app/Filament/Widgets/UsersChart.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\User;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class UsersChart extends ChartWidget
+{
+    protected static ?string $heading = 'Users Chart';
+
+    protected function getData(): array
+    {
+        // https://github.com/Flowframe/laravel-trend
+        // https://filamentphp.com/docs/2.x/admin/dashboard/charts
+        $data = Trend::model(User::class)
+            ->between(now()->subYear(), now())
+            ->perMonth()
+            ->count();
+
+        return [
+            'labels' => $data->map(fn(TrendValue $value) => $value->date),
+            'datasets' => [
+                [
+                    'label' => 'Users',
+                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
+                ],
+            ],
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
+
+
